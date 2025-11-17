1a7e72116ea6250d12149d143c8ce478add5b770
Jason Cho <jcho111@ucsc.edu>
2025-11-17 22:59:52 +0000

    Step 1-3: Set up Leaflet dependencies

---
+++ PLAN.md
@@ -0,0 +1,10 @@
+# PLAN FOR D3.a
+
+1. Read and understand main.ts
+2. Replace main.ts with my own code
+3. Set up Leaflet dependencies
+4. Initialize grids to be approximately the size of a building
+5. Initialize contents of a cell to have tokens
+6. Make a graphic showing the contents within a cell (tokens and what value they have)
+7. Make the cells clickable and interactable
+8. Spawn tokens in cells using the luck function
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-// @deno-types="npm:@types/leaflet"
 import leaflet from "leaflet";
 
 // Style sheets
@@ -9,7 +8,6 @@ import "./style.css"; // student-controlled page style
 import "./_leafletWorkaround.ts"; // fixes for missing Leaflet images
 
 // Import our luck function
-import luck from "./_luck.ts";
 
 // Create basic UI elements
 
@@ -63,53 +61,8 @@ playerMarker.addTo(map);
 
 // Display the player's points
 let playerPoints = 0;
-statusPanelDiv.innerHTML = "No points yet...";
+statusPanelDiv.innerHTML = "Points: 0";
 
-// Add caches to the map by cell numbers
-function spawnCache(i: number, j: number) {
-  // Convert cell numbers into lat/lng bounds
-  const origin = CLASSROOM_LATLNG;
-  const bounds = leaflet.latLngBounds([
-    [origin.lat + i * TILE_DEGREES, origin.lng + j * TILE_DEGREES],
-    [origin.lat + (i + 1) * TILE_DEGREES, origin.lng + (j + 1) * TILE_DEGREES],
-  ]);
 
-  // Add a rectangle to the map to represent the cache
-  const rect = leaflet.rectangle(bounds);
-  rect.addTo(map);
 
-  // Handle interactions with the cache
-  rect.bindPopup(() => {
-    // Each cache has a random point value, mutable by the player
-    let pointValue = Math.floor(luck([i, j, "initialValue"].toString()) * 100);
 
-    // The popup offers a description and button
-    const popupDiv = document.createElement("div");
-    popupDiv.innerHTML = `
-                <div>There is a cache here at "${i},${j}". It has value <span id="value">${pointValue}</span>.</div>
-                <button id="poke">poke</button>`;
-
-    // Clicking the button decrements the cache's value and increments the player's points
-    popupDiv
-      .querySelector<HTMLButtonElement>("#poke")!
-      .addEventListener("click", () => {
-        pointValue--;
-        popupDiv.querySelector<HTMLSpanElement>("#value")!.innerHTML =
-          pointValue.toString();
-        playerPoints++;
-        statusPanelDiv.innerHTML = `${playerPoints} points accumulated`;
-      });
-
-    return popupDiv;
-  });
-}
-
-// Look around the player's neighborhood for caches to spawn
-for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
-  for (let j = -NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
-    // If location i,j is lucky enough, spawn a cache!
-    if (luck([i, j].toString()) < CACHE_SPAWN_PROBABILITY) {
-      spawnCache(i, j);
-    }
-  }
-}
